9b8aceb8389523eb23fed801cb69d253a5c00ccd
alsupica <alsupica91@gmail.com>
2021-12-10 15:29:47 +0100

    finished course on loops

--- js_course/chapter1/loops.js
+++ js_course/chapter1/loops.js
@@ -17,7 +17,7 @@
 
 //NEWWER VERSION OF FOR LOOPS
 
-const names = [ `Krow`, `Libish`, `Love`, `Neko`, `Richou`, `Doomsi` ];
+// const names = [ `Krow`, `Libish`, `Love`, `Neko`, `Richou`, `Doomsi` ];
 
 // for (let name of names) {
 // 	// console.log(name); this will input all  the names
@@ -28,10 +28,38 @@ const names = [ `Krow`, `Libish`, `Love`, `Neko`, `Richou`, `Doomsi` ];
 //forEach is 20% SLOWER than for, use only when REQUIRED
 // because you're inside a funciton you can't use break
 // also exit keyword seem to not work
-names.forEach(function(name, index) {
-	if (name === `Neko`) {
-		console.log(`badabim badaboum`);
-		// stop;
-	}
-	console.log(name, index);
-});
+// names.forEach(function(name, index) {
+// 	if (name === `Neko`) {
+// 		console.log(`badabim badaboum`);
+// 		// stop;
+// 	}
+// 	console.log(name, index);
+// });
+
+// ____________________ FOR IN AND WHILE LOOPS __________________
+// for of is used in arrays, for in is used in objects
+// const user = {
+// 	name: `Krow`,
+// 	age: 25,
+// 	sub: 200,
+// 	money: `lolno`
+// }; // QUICK REMINDER : in objects, the thing on the left is called "key"
+// and the thing on the right is called "value"
+// and the whole thing is a "property"
+
+// for (let x in user) {
+// 	console.log(x); // this will show the key;
+// 	console.log(user[x]); // this will return value;
+// }
+
+//WHILE, DO WHILE
+//while is the same than C
+//do while is almost the same but will run at least once even if
+//statement is false
+
+let i = 0;
+
+do {
+	console.log(i);
+	i++;
+} while (i > 10);
