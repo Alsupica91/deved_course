900b9dce9c9fdad3e43c665eb40da0795bc3a15c
alsupica <alsupica91@gmail.com>
2021-12-10 14:18:35 +0100

    finished on switch cases

--- js_course/chapter1/switch.js
+++ js_course/chapter1/switch.js
@@ -1,1 +1,43 @@
-const text = `banana`;
+// // switch cases are used to trim if forests
+
+// const text = `banana3`;
+
+// // if (text === `banana`) {
+// // 	// code
+// // } else if (text === `apple`) {
+// // 	// code
+// // }
+
+// // tout le code apres le bon case est exec, use break;
+// switch (text) {
+// 	case `banana1`:
+// 		console.log('whatever');
+// 	case `banana2`:
+// 		console.log('whatever');
+// 	case `banana3`:
+// 		console.log('whatever');
+// 	case `banana`:
+// 		console.log('whatever');
+// 		break;
+// 	case `apple`:
+// 		console.log(`whatever2`);
+// 	default:
+// 		console.log(`what fruit is dat ?`);
+// }
+
+let userInput = prompt(`Tell me a fruit`);
+let convertedValue = userInput.toLowerCase();
+
+switch (convertedValue) {
+	case `kiwi`:
+		console.log(`kiwi fun fact`);
+		break;
+	case `banana`:
+		console.log(`banana fun fact`);
+		break;
+	case `apple`:
+		alert(`apple fun fact`);
+		break;
+	default:
+		console.log(`no fruit`);
+}
