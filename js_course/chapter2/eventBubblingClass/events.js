24054cd6bf6ffea90ffd2514796c15127f5a527b
alsupica <alsupica91@gmail.com>
2021-12-13 14:38:45 +0100

    reinstalled Prettier

--- js_course/chapter2/eventBubblingClass/events.js
+++ js_course/chapter2/eventBubblingClass/events.js
@@ -4,43 +4,28 @@ const todoNr = document.querySelector('.todo-nr b');
 const items = todoList.children;
 const mainTitle = document.querySelector('.main-title');
 
-//ATTACH AN EVENT LESTENER
-//quand on utilise funtion() juste en dessous. on dit 'Anonymous' funtion, c'est juste
-//pour dire qu'elle est defini en dessous. Ca sert si on utilise qu'une fois la
-//fonction directement et pas de multiple fois ailleur dans le code
-// button.addEventListener('click', addItem); // si on met des () a addItem, elle run automatiquement, sans devoir cliquer
+button.addEventListener('click', function() {
+	const newItem = document.createElement('li');
+	newItem.classList.add('item');
+	newItem.innerText = `Item ${items.length + 1}`;
+	todoList.appendChild(newItem);
+	todoNr.innerText = items.length;
+	newItem.addEventListener('click', deleteItem);
+});
 
-// button.addEventListener('click', function() {
-// 	const newItem = document.createElement('li');
-// 	newItem.classList.add('item');
-// 	newItem.innerText = `Item ${items.length + 1}`;
-// 	console.log(newItem.classList);
-// 	todoList.appendChild(newItem);
-// 	todoNr.innerText = items.length;
-// });
+// for (item of items) {
+// 	item.addEventListener('click', deleteItem);
+// }
 
-// function addItem() {
-// 	console.log('added a new item');
-// } 	USE FOR EXEMPLE ABOVE
+// when you run an event, the first argument of the function will be reserved to the event
+//so you could use deleteItem(whatever) where "whatever" would store the event info
+// here, you can use e.target to specificaly select what you clicked on
+function deleteItem(e) {
+	// console.log(e.target);
+	e.stopPropagation(); // this is how you stop event bubbling up
+	e.target.remove();
+}
 
-//With this you can acces individual style and change it based on event
-// button.addEventListener('click', function() {
-// 	mainTitle.style.color = 'red';
-// 	mainTitle.style.fontSize = '50px';
-// });
-
-//You can also add a class that you would have styled with css !
-// on peut ajouter event dans funciton() pour choper des infos sur l'event choisis
-// button.addEventListener('click', function(event) {
-// 	console.log(event);
-// mainTitle.classList.add('color'); this is to perma add
-// mainTitle.classList.toggle('color'); // this add or remove
-// });
-
-//same but with keyboard input
-button.addEventListener('keydown', function(event) {
-	console.log(event);
-	if (event.keyCode === 32) {
-		mainTitle.classList.toggle('color');
-	}
+todoList.addEventListener('click', function() {
+	todoList.classList.toggle('fade');
 });
