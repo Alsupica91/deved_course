3decbdf64f2320471672eb1c11c877aec5207c44
alsupica <alsupica91@gmail.com>
2021-12-14 20:56:05 +0100

    finished 4.2 class

---
+++ js_course/arraysCourse/array2.js
@@ -0,0 +1,19 @@
+//declare random array
+const list = ["qw", "as", "zx", "ty", "gh"];
+
+list.forEach(function (item) {
+  console.log(item);
+});
+
+//HIGHER ORDER FUNCTION
+// function repeater(fn) {
+//   fn();
+//   fn();
+//   fn();
+// }
+
+// function sayHello() {
+//   console.log("Hello there");
+// }
+
+// repeater(sayHello);
