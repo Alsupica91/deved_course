d35f53dd39b01bf515bedea454a88de059430f46
alsupica <alsupica91@gmail.com>
2022-01-14 14:15:32 +0100

    finished pads animation

---
+++ js_course/beatMakerProject/app.js
@@ -0,0 +1,55 @@
+class DrumKit {
+  constructor() {
+    this.pads = document.querySelectorAll(".pad");
+    this.playBtn = document.querySelector(".play");
+    this.kickAudio = document.querySelector(".kick-sound");
+    this.snareAudio = document.querySelector(".snare-sound");
+    this.hihatAudio = document.querySelector(".hihat-sound");
+    this.index = 0;
+    this.bpm = 120;
+  }
+  activePad() {
+    this.classList.toggle("active");
+  }
+  repeat() {
+    let step = this.index % 8;
+    const activeBars = document.querySelectorAll(`.b${step}`);
+    // Loop over the pads
+    activeBars.forEach((bar) => {
+      bar.style.animation = `playTrack 0.3s alternate ease-in-out 2`;
+      // Check if pads are active
+      if (bar.classList.contains("active")) {
+        // Check each sounds
+        if (bar.classList.contains("kick-pad")) {
+          this.kickAudio.play();
+        }
+        if (bar.classList.contains("snare-pad")) {
+          this.snareAudio.play();
+        }
+        if (bar.classList.contains("hihat-pad")) {
+          this.hihatAudio.play();
+        }
+      }
+    });
+    this.index++;
+  }
+  start() {
+    const interval = (60 / this.bpm) * 1000; // bpm c'est par min, donc 60/bmp pour avoir par seconde, mais setInterval utilise des ms, donc * 1000
+    setInterval(() => {
+      this.repeat();
+    }, interval);
+  }
+}
+
+const drumKit = new DrumKit();
+
+drumKit.pads.forEach((pad) => {
+  pad.addEventListener("click", drumKit.activePad);
+  pad.addEventListener("animationend", function () {
+    this.style.animation = "";
+  });
+});
+
+drumKit.playBtn.addEventListener("click", function () {
+  drumKit.start();
+});
